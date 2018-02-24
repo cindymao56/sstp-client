c9d4677c6db7a6499232f4b99b014fec33c234b1
eivnaes <eivnaes@17c00af8-5a35-4a21-99a1-c4d57c7ef0cf>
2018-02-24 18:27:26 +0000

    Optimizing SSL performance

--- trunk/src/sstp-client.c
+++ trunk/src/sstp-client.c
@@ -35,6 +35,26 @@
 #include "sstp-private.h"
 #include "sstp-client.h"
 
+/*! OpenSSL cipher suites
+ * 
+ * https://wiki.mozilla.org/Security/Server_Side_TLS
+ * Intermediate compatibility (default), as SSTP appeared only in WinSrv2k8
+ */
+
+static const char* const sstp_client_ssl_ciphers =
+    "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305" \
+    ":ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256" \
+    ":ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384" \
+    ":DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384" \
+    ":ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA" \
+    ":ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384" \
+    ":ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256" \
+    ":DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA" \
+    ":ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA" \
+    ":AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256" \
+    ":AES128-SHA:AES256-SHA:DES-CBC3-SHA" \
+    ":!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4:!DSS";
+
 /*! Global context for the sstp-client */
 static sstp_client_st client;
 
@@ -479,11 +499,44 @@ static status_t sstp_init_ssl(sstp_client_st *client, sstp_option_st *opt)
         goto done;
     }
 
-    /* Configure the crypto options, eliminate SSLv2 */
-    status = SSL_CTX_set_options(client->ssl_ctx, SSL_OP_ALL|SSL_OP_NO_SSLv2);
+    /* Configure the crypto options, eliminate SSLv2, SSLv3 */
+    status = SSL_CTX_set_options(
+        client->ssl_ctx,
+        SSL_OP_ALL |
+            SSL_OP_NO_SSLv2 |
+            SSL_OP_NO_SSLv3);
     if (status == -1)
     {
         log_err("Could not set SSL options");
+        goto done;
+    }
+
+#ifdef SSL_OP_NO_COMPRESSION
+    /* disable to mitigate CRIME attack */
+    status = SSL_CTX_set_options(client->ssl_ctx, SSL_OP_NO_COMPRESSION);
+    if (status == -1)
+    {
+        log_err("Could not set SSL options");
+        goto done;
+    }
+#endif
+
+#ifdef SSL_MODE_RELEASE_BUFFERS
+    /* reduce idle connection memory usage */
+    status = SSL_CTX_set_mode(client->ssl_ctx, SSL_MODE_RELEASE_BUFFERS);
+    if (status == -1)
+    {
+        log_err("Could not set SSL options");
+        goto done;
+    }
+#endif
+
+    status = SSL_CTX_set_cipher_list(
+        client->ssl_ctx,
+        sstp_client_ssl_ciphers);
+    if (status != 1)
+    {
+        log_err("Could not set SSL ciphersuites");
         goto done;
     }
 
